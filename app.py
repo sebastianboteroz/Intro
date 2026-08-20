18209a70e81402863fa0bd54210cf5b2ff397941
sebastianboteroz <sebastianboteroz2007@gmail.com>
2026-08-20 07:24:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,5 +9,10 @@ image = Image.open("piedras.jpg")
 st.image(image, caption="Interfaces Multimodales")
 
 
-texto = st.text_input("Escribe algo", "Este es mi texto")
-st.write("El texto escrito es", texto)
+texto = st.text_input('Escribe algo', 'Este es mi texto')
+st.write('El texto escrito es', texto)
+
+st.subheader("Ahora usemos 2 columnas")
+
+
+col1, col2= st.columns(2)
