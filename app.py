0f689814582c63f280780ea5f48c4f3e87fd7593
sebastianboteroz <sebastianboteroz2007@gmail.com>
2026-08-20 07:13:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import streamlit as st
+from PIL import Image
 
 st.title("La primera app de Sebastián en Streamlit")
+
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales")
+st.write("Facilmente puedo realizar backend y frontend")
+image = Image.open("")
+st.image(image, caption="Interfaces Multimodales")
