66050be52a342bb22fc42c1ffbe18d5c04eee83a
sebastianboteroz <sebastianboteroz2007@gmail.com>
2026-08-20 07:27:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -14,5 +14,16 @@ st.write('El texto escrito es', texto)
 
 st.subheader("Ahora usemos 2 columnas")
 
-
 col1, col2= st.columns(2)
+
+with col1:
+  st.subheader("Esta es la primera columna")
+  st.write("Las interfaces multimodales mejoran la experienica de usuario")
+  resp = st.checkbox('Estoy de acuerdo')
+  if resp: 
+  st.write('Correcto!')
+
+with col2: 
+  st.subheader("Esta es la segunda columna")
+  modo = st.radio
+
