11b0378d907327a2f72ff6e390c5ba37ca9a032b
sebastianboteroz <sebastianboteroz2007@gmail.com>
2026-08-20 07:15:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ st.title("La primera app de Sebastián en Streamlit")
 
 st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales")
 st.write("Facilmente puedo realizar backend y frontend")
-image = Image.open("")
+image = Image.open("piedras.jpg")
 st.image(image, caption="Interfaces Multimodales")
